8aba9e3db4d0250ae103038aec0eb09cba668553
kienjoe22 <cool_joe92@hotmail.com>
2015-04-13 00:20:53 +0800

    update delete customer

--- P_DELETE_/P_DELETE_CUSTOMER.sql
+++ P_DELETE_/P_DELETE_CUSTOMER.sql
@@ -3,10 +3,15 @@ PROCEDURE P_DELETE_CUSTOMER(
 IN_CD IN T_MT_CUSTOMER.CD%TYPE
 )
 IS
+<<<<<<< HEAD
 
 
 BEGIN
 
+=======
+
+BEGIN
+>>>>>>> origin/master
 
 DELETE FROM T_MT_CUSTOMER WHERE CD = IN_CD;
 
